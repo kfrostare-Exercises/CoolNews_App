26459ad10e62b498d2be3a3d161059cf37b988d4
karolina <karolina.frostare@gmail.com>
2020-03-26 05:32:34 +0100

    creates and modifies Article Model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,4 +1,4 @@
-Rspec.describe Article, type: :model do
+RSpec.describe Article, type: :model do
   describe 'DB table' do
     it { is_expected.to have_db_column :id }
     it { is_expected.to have_db_column :title}
